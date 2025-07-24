28697d67c46caa7e79b83835bec7b2ac92b61a70
shusank <shusank.chaudhary@gmail.com>
2025-07-24 17:40:59 +0545

    intial commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ 
+ 
  const parent = React.createElement("div", {id:"parent"}, 
  [React.createElement("div", {id:"child"},),
- [React.createElement("h1" , {} , "I'm H1 tag"), React.createElement("h2", {}, "I'm tag h2")],
+ [React.createElement("h1" , {} , "I'm H1 tag🚀"), React.createElement("h2", {}, "I'm tag h2")],
  React.createElement("div", {id:"child2"},),
  [React.createElement("h1" , {} , "I'm H1 tag"), React.createElement("h2", {}, "I'm tag h2")]]
 
